bddd49ab80fb93470247e491612b14cbc8b0a773
Timilehin Adeleye <timmyadet@gmail.com>
2023-02-22 13:09:56 -0800

    functions and nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,10 +14,7 @@ void print_times_table(int n)
 
 	if (n > 15 || n < 0)
 	{
-		return;
-	}
-	else
-	{
+
 		for (num = 0; num <= n; num++)
 		{
 			_putchar('0');
@@ -42,7 +39,8 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar((prod /10) % 10 + '0');
+					_putchar((prod / 10) % 10 + '0');
+				}
 				else if (prod <= 99 && prod >= 10)
 				{
 					_putchar((prod / 10) + '0');
