f60c081e505a40eb53986c51645e0bed95a9b12e
Timilehin Adeleye <timmyadet@gmail.com>
2023-02-22 12:39:38 -0800

    functions and nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_times_table - function that prints n times table
+ * @n: n is the number to be multiplied
+ *
+ * Return: null if n is greater than 15 or less than 0
+ *
+*/
+
+void print_times_table(int n)
+{
+	int num, mult, prod;
+
+	if (n > 15 || n < 0)
+	{
+		return (' ');
+	}
+	else
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 9)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all the multiples of 3 and 5 below 1024
+ *
+ * Return: sum of the values
+*/
+
+int main(void)
+{
+	int n;
+	int sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
