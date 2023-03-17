1629b49abff912a63f3d5142e558fc637f7e1c9c
Timilehin Adeleye <timmyadet@gmail.com>
2023-03-17 14:44:57 -0700

    More Malloc and Free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,40 +16,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 != NULL)
 	{
 		while (s1[s1_len] != '\0')
-		{
 			s1_len++;
-		}
 	}
-
 	if (s2 != NULL)
 	{
 		while (s2[s2_len] != '\0')
-		{
 			s2_len++;
-		}
 	}
-
 	if (n >= s2_len)
-	{
 		n = s2_len;
-	}
 
 	result = malloc(sizeof(char) * (s1_len + n + 1));
-
 	if (result == NULL)
-	{
 		return (NULL);
-	}
+
 	for (i = 0; i < s1_len; i++)
-	{
 		result[i] = s1[i];
-	}
+
 	for (j = 0; j < n; j++)
-	{
 		result[i + j] = s2[j];
-	}
-
 	result[i + j] = '\0';
-
 	return (result);
 }
