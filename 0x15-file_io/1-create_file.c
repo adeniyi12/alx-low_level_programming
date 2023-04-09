1450d2e1e5e0aa1ba32a15c95c9e8d9db627667b
Timilehin Adeleye <timmyadet@gmail.com>
2023-04-09 13:13:17 -0700

    File I_O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,29 +7,26 @@
  * Return: 1 on success, -1 on failure
 */
 
-#include "main.h"
-
 int create_file(const char *filename, char *text_content)
 {
-	if (!filename)
+	int o, w, len = 0;
+
+	if (filename == NULL)
 		return (-1);
 
-	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd < 0)
-		return (-1);
-
-	int len = 0;
-
-	if (text_content)
+	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
-			;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	ssize_t bytes_written = write(fd, text_content, len);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	close(fd);
+	if (o == -1 || w == -1)
+		return (-1);
 
-	return ((bytes_written == len) ? 1 : -1);
+	close(o);
+
+	return (1);
 }
